a76bd630bd97d5f1e07dc3a79ef37307a30c8d3f
kokrah <kwame.okrah@gmail.com>
2015-02-27 17:06:37 -0500

    changed fct. residuals to compResids

---
+++ R/compResids.r
@@ -0,0 +1,17 @@
+#' Compute residuals
+#' Notes:
+#' 1. 
+#' Output:
+#' returns a matrix of residuals
+#' @param y matrix of median adjusted quantiles (genes by samples)
+#' @param groups groups factor / character
+#' @param lambda shrinkage parameter
+#' @param levels (optional) specify order of levels
+#' @export
+compResids = function (y, groups, lambda, levels=NULL) {
+    
+  fitModel = fitCoeffs(y, groups, lambda, levels)
+  
+  y - predicted(fitModel$design, fitModel$betas)
+    
+}
